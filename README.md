1c7f0b10269d31016599aceeeab2e4acd3513bb7
swapneelyashwantgosavi <swap24gosavi@gmail.com>
2022-11-01 15:56:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Food Mobile Application
+
+## Screen Animation :
+
+<table><tr><td>
+
+![Alt text](for_readme/food.gif "Screens")
+
+</td></tr></table>
---
+++ src/api/yelp.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization:
+      "Bearer YELP_TOKEN",
+  },
+});
